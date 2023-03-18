de77080ca1993d62f2edfdfc02a8843a456f1f49
TaniaPandal <psiqonautas@gmail.com>
2023-03-18 20:46:35 +0100

    updating composer(fixed 51 errors)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(LoginRequest $request)
     {
 
     }
 
-    public function signup()
+    public function signup(SignupRequest $request )
     {
 
     }
 
-    public function logout()
+    public function logout($request)
     {
-        
+
     }
 }   
 
