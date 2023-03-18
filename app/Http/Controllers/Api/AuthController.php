dc1490ae16a5ff81b386abe38f41dbcd629042c2
TaniaPandal <psiqonautas@gmail.com>
2023-03-18 20:06:10 +0100

    Requests creates

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
-}
+    public function login()
+    {
+
+    }
+
+    public function signup()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
+}   
+
