abe5e2aef626b8d85f66bbe8b823fb7844648c55
TaniaPandal <psiqonautas@gmail.com>
2023-03-19 18:34:59 +0100

    fixing sanctum

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,22 +5,55 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
+      $data = $request->validated();
+      if(Auth::atempt($data)) {
+        return response ([
+            'message'=> 'Email o contraseña incorrectos'
+        ]);
+      }
+      /**@var User $user */
+      $user = Auth::user();
+      $token = $user->createToken('main')->plainTextToken;
+      return response([
+        'user'=>$user,
+        'token'=>$token
+    ]);
 
     }
 
     public function signup(SignupRequest $request )
     {
+        $data = $request->validated();
+        $user = User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=>$token
+        ]);
 
     }
 
     public function logout($request)
     {
+        /**@var User $user */
+        $user = $request->user();
+
+
 
     }
 }   
--- config/app.php
+++ config/app.php
@@ -181,10 +181,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+       
 
         /*
          * Package Service Providers...
          */
+        Laravel\Sanctum\SanctumServiceProvider::class,
 
         /*
          * Application Service Providers...
